2cdde91835005588c15ada232e9051a1cfe8bae5
Bauti <biscayartbautista@gmail.com>
2026-04-14 18:47:32 -0300

    Agrego ejercicio 1 - Variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,12 @@
+const nombre = "Bautista";
+const edad = 20;
+const curso = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${curso}.`);
+
+let contador = 0;
+contador ++;
+contador ++; 
+contador ++;
+
+console.log(`El contador tiene el valor: ${contador}`);
